44c34a950a173b8fc5015f959cfc052e28c7eefd
joeevon <372804854@qq.com>
2017-09-24 23:05:08 +0800

    commit

---
+++ WxPayAPI_JAVA_v3/java-sdk-v3/src/test/java/com/github/wxpay/sdk/WXPayConfigImpl.java
@@ -0,0 +1,81 @@
+package com.github.wxpay.sdk;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class WXPayConfigImpl extends WXPayConfig{
+
+    private byte[] certData;
+    private static WXPayConfigImpl INSTANCE;
+
+    private WXPayConfigImpl() throws Exception{
+        String certPath = "D://CERT/common/apiclient_cert.p12";
+        File file = new File(certPath);
+        InputStream certStream = new FileInputStream(file);
+        this.certData = new byte[(int) file.length()];
+        certStream.read(this.certData);
+        certStream.close();
+    }
+
+    public static WXPayConfigImpl getInstance() throws Exception{
+        if (INSTANCE == null) {
+            synchronized (WXPayConfigImpl.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new WXPayConfigImpl();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public String getAppID() {
+        return "wx7d69cc6664fea758";
+    }
+
+    public String getMchID() {
+        return "1488647162";
+    }
+
+    public String getKey() {
+        return "3foptz6c3zk3lh28jd5vpu0q8y4umnai";
+    }
+
+    public InputStream getCertStream() {
+        ByteArrayInputStream certBis;
+        certBis = new ByteArrayInputStream(this.certData);
+        return certBis;
+    }
+
+
+    public int getHttpConnectTimeoutMs() {
+        return 2000;
+    }
+
+    public int getHttpReadTimeoutMs() {
+        return 10000;
+    }
+
+    IWXPayDomain getWXPayDomain() {
+        return WXPayDomainSimpleImpl.instance();
+    }
+
+    public String getPrimaryDomain() {
+        return "api.mch.weixin.qq.com";
+    }
+
+    public String getAlternateDomain() {
+        return "api2.mch.weixin.qq.com";
+    }
+
+    @Override
+    public int getReportWorkerNum() {
+        return 1;
+    }
+
+    @Override
+    public int getReportBatchSize() {
+        return 2;
+    }
+}
