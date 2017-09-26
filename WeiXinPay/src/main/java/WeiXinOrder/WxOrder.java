9ff147ffe5c317a44287eacf339737970a0834e9
joeevon <372804854@qq.com>
2017-09-26 22:01:55 +0800

    commit

---
+++ WeiXinPay/src/main/java/WeiXinOrder/WxOrder.java
@@ -0,0 +1,59 @@
+/**
+ * Created by joee on 2017/9/26.
+ */
+
+package WeiXinOrder;
+
+import sdk.WXPay;
+import sdk.WXPayConfigImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WxOrder {
+    private WXPay wxpay;
+    private WXPayConfigImpl config;
+
+    public WxOrder() throws Exception {
+        config = WXPayConfigImpl.getInstance();
+        wxpay = new WXPay(config);
+    }
+
+    private String GetTradeNo() {
+        return "201413091059590000003433-asd005";
+    }
+
+    private String GetExpireTime() {
+        return "20170930000000";
+    }
+
+    public String UnifiedOrder() {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("body", "益爱海蓓课程支付");
+        data.put("out_trade_no", GetTradeNo());
+        data.put("fee_type", "CNY");
+        data.put("total_fee", "1");
+        data.put("spbill_create_ip", "0.0.0.0");
+        data.put("notify_url", "http://test.letiantian.me/wxpay/notify");
+        data.put("trade_type", "JSAPI");
+        data.put("time_expire", GetExpireTime());
+        data.put("openid", "oMDigwQEv-nmXH29CIt0Hx5uCw3o");
+
+        try {
+            Map<String, String> r = wxpay.unifiedOrder(data);
+            System.out.println(r);
+            return r.get("prepay_id");
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("WxOrder begin.\n");
+        WeiXinOrder.WxOrder order = new WeiXinOrder.WxOrder();
+        order.UnifiedOrder();
+        System.out.println("\nWxOrder end.");
+    }
+}
